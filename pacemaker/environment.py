efab192986afff16900a443507185918a9735cc0
JamesMClarke <jamesmorganclarke@hotmail.co.uk>
2022-03-03 18:10:44 +0000

    feat(environment):Basic class structure

--- pacemaker/environment.py
+++ pacemaker/environment.py
@@ -1,9 +1,28 @@
+import pacemaker, accelerometer
+
 class Environment:     
-    def is_finished():
+    #Set the unit of time
+    unit_of_time = 0.1
+    time = 0
+    accelerometer = False
+    def __init__(self):
+        self.pacemaker = pacemaker()
+
+    def is_finished(self):
         pass
 
-    def add_accelerometer():
-        pass
+    def add_accelerometer(self):
+        """ Creates an accelerometer """
+        self.accelerometer = accelerometer()
 
-    def get_time():
-        pass
+    def get_accelerometer(self):
+        """ Returns the accelerometer """
+        return(self.accelerometer)
+
+    def get_time(self):
+        """ Return current time """
+        return(self.time)
+
+    def advance_time(self):
+        """ Basic use of advancing time """
+        self.time += self.unit_of_time
