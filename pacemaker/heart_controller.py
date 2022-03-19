ca02bbcf33540851581ab2966188469d71832094
Chenyu <machenyu96@gmail.com>
2022-03-19 10:57:42 +0000

    feat:update the class HeartController

--- pacemaker/heart_controller.py
+++ pacemaker/heart_controller.py
@@ -2,7 +2,6 @@ import _thread
 from time import sleep
 import IO
 
-
 class HeartController:
     # from parameters import Parameters
 
@@ -14,297 +13,94 @@ class HeartController:
     #
     # )
 
-    def AAT(self, data: IO):
-        # data.lower_rate_limit
-        # data.upper_rate_limit
-        # data.atrial_amplitude
-        # data.atrial_pulse_width
-        # data.atrial_sensitivity,
-        # data.ARP,
-        # data.PVARP
-        interval = self.pace_atrium(data.lower_rate_limit)
-        # todo get P_wave / heart_rate
-        p_wave = True
-        to_trigger = self.sense_atrium(data,50,interval,p_wave,"trigger")
-        if to_trigger:
-            self.trigger(data.atrial_amplitude,data.atrial_pulse_width)
-        else:
-            sleep(data.atrial_pulse_width/1000)
-
-        return data.atrial_amplitude,data.atrial_pulse_width
-
-
-    # while True:
-    #     # sensed ventricle
-    #     if sensed_ventricle:
-    #         continue
-    #     interval = 60000
-    #
-    #     pacemaker.ARP
-    #
-    # interval = 60000 / pacemaker.lower_rate_limit
-    #
-    # return pacemaker.atrial_amplitude, pacemaker.atrial_pulse_width,pacemaker.atrial_sensitivity
-
-    def VVT(self,data:IO):
-            # pacemaker.lower_rate_limit,
-            # pacemaker.upper_rate_limit,
-            # pacemaker.ventricular_amplitude,
-            # pacemaker.ventricular_pulse_width,
-            # pacemaker.ventricular_sensitivity,
-            # pacemaker.VRP
-            interval = self.pace_ventricle(data.lower_rate_limit)
-            # todo get r_wave / heart_rate
-            r_wave = 1000
-            to_trigger = self.sense_ventricle(data, 70, interval, r_wave, "trigger")
-            if to_trigger:
-                self.trigger(data.ventricular_amplitude,data.ventricular_pulse_width)
-            else:
-                sleep(data.ventricular_pulse_width/1000)
-
-    def AOO(self,data:IO):
-        # pacemaker.lower_rate_limit,
-        # pacemaker.upper_rate_limit,
-        # pacemaker.atrial_amplitude,
-        # pacemaker.atrial_pulse_width,
-
-        interval = self.pace_atrium(data.lower_rate_limit) - data.atrial_pulse_width
-        sleep(interval/1000)
-        print(data.atrial_amplitude,data.atrial_pulse_width)
-        return data.atrial_amplitude,data.atrial_pulse_width
-
-
-        # return li
-        # todo 两个参数（a_amplitude,a_pulse_width)
-
-    def AAI(self,data):
-        #     pacemaker.lower_rate_limit,
-        #     pacemaker.upper_rate_limit,
-        #     pacemaker.atrial_amplitude,
-        #     pacemaker.atrial_pulse_width,
-        #     pacemaker.atrial_sensitivity,
-        #     pacemaker.ARP,
-        #     pacemaker.PVARP,
-        #     pacemaker.hysteresis,
-        #     pacemaker.rate_smoothing
-        interval = self.pace_atrium(data.lower_rate_limit)
-        # todo get p_wave / heart_rate
-        p_wave = 1000
-        to_trigger = self.sense_atrium(data, 70, interval, p_wave,"inhibit")
-        # 
-        if not to_trigger：
-
-
-    def VOO(self,data:IO):
-        # pacemaker.lower_rate_limit,
-        # pacemaker.upper_rate_limit,
-        # pacemaker.ventricular_amplitude,
-        # pacemaker.ventricular_pulse_width
-        interval = self.pace_ventricle(data.lower_rate_limit) - data.ventricular_pulse_width
-        sleep(interval/1000)
-        print(data.ventricular_amplitude,data.ventricular_pulse_width)
-        return data.ventricular_amplitude,data.ventricular_pulse_width
-
-    def VVI(self,pacemaker):
-        self.pace_ventricle()
-        self.sense_ventricle()
-        self.response_inhibit()
-        # return [
-        #     pacemaker.lower_rate_limit,
-        #     pacemaker.upper_rate_limit,
-        #     pacemaker.ventricular_amplitude,
-        #     pacemaker.ventricular_pulse_width,
-        #     pacemaker.ventricular_sensitivity,
-        #     pacemaker.VRP,
-        #     pacemaker.hysteresis,
-        #     pacemaker.rate_smoothing,
-        # ]
-
-    def VDD(self,pacemaker):
-        self.pace_ventricle()
-        self.sense_ventricle()
-        self.sense_atrium()
-        self.response_trigger()
-        self.response_inhibit()
-        # return [
-        #     pacemaker.lower_rate_limit,
-        #     pacemaker.upper_rate_limit,
-        #     pacemaker.fixed_AV_delay,
-        #     pacemaker.dynamic_AV_delay,
-        #     pacemaker.ventricular_amplitude,
-        #     pacemaker.ventricular_pulse_width,
-        #     pacemaker.VRP,
-        #     pacemaker.PVARP_extension,
-        #     pacemaker.rate_smoothing,
-        #     pacemaker.atr_duration,
-        #     pacemaker.atr_fallback_mode,
-        #     pacemaker.atr_fallback_time
-        # ]
-        # pass
-
-    def DOO(self,data):
-        try:
-            _thread.start_new_thread(self.AOO, (data,))
-            _thread.start_new_thread(self.VOO,(data,))
-        except Exception as ex:
-            print(ex)
-        # return [
-        #     pacemaker.lower_rate_limit,
-        #     pacemaker.upper_rate_limit,
-        #     pacemaker.fixed_av_delay,
-        #     pacemaker.atrial_amplitude,
-        #     pacemaker.ventricular_amplitude,
-        #     pacemaker.atrial_pulse_width,
-        #     pacemaker.ventricular_pulse_width
-        #
-        # ]
-
-    def DDI(self,pacemaker):
-        self.pace_atrium()
-        self.pace_ventricle()
-        self.sense_atrium()
-        self.sense_ventricle()
-        self.response_inhibit()
-        # return [
-        #     pacemaker.lower_rate_limit,
-        #     pacemaker.upper_rate_limit,
-        #     pacemaker.fixed_AV_delay,
-        #     pacemaker.atrial_amplitude,
-        #     pacemaker.ventricular_amplitude,
-        #     pacemaker.atrial_pulse_width,
-        #     pacemaker.ventricular_pulse_width,
-        #     pacemaker.atrial_sensitivity,
-        #     pacemaker.ventricular_sensitivity,
-        #     pacemaker.VRP,
-        #     pacemaker.ARP,
-        #     pacemaker.PVARP,
-        # ]
-
-    def DDD(self,pacemaker):
-        self.pace_ventricle()
-        self.pace_ventricle()
-        self.sense_atrium()
-        self.sense_ventricle()
-        self.response_trigger()
-        self.response_inhibit()
-        # return [
-        #     pacemaker.lower_rate_limit,
-        #     pacemaker.upper_rate_limit,
-        #     pacemaker.fixed_AV_delay,
-        #     pacemaker.dynamic_AV_delay,
-        #     pacemaker.sensed_av_delay_offset,
-        #     pacemaker.atrial_amplitude,
-        #     pacemaker.ventricular_amplitude,
-        #     pacemaker.atrial_pulse_width,
-        #     pacemaker.ventricular_pulse_width,
-        #     pacemaker.atrial_sensitivity,
-        #     pacemaker.ventricular_sensitivity,
-        #     pacemaker.VRP,
-        #     pacemaker.ARP,
-        #     pacemaker.PVARP,
-        #     pacemaker.PVARP_extension,
-        #     pacemaker.hysteresis,
-        #     pacemaker.rate_smoothing,
-        #     pacemaker.atr_duration,
-        #     pacemaker.atr_fallback_mode,
-        #     pacemaker.atr_fallback_time
-        # ]
-        # pass
-    #
-    # def AOOR():
-    #     pass
-    #
-    # def AAIR():
-    #     pass
-    #
-    # def VOOR():
-    #     pass
-    #
-    # def VVIR():
-    #     pass
-    #
-    # def VDDR():
-    #     pass
-    #
-    # def DOOR():
-    #     pass
-    #
-    # def DDIR():
-    #     pass
-    #
-    # def DDDR():
-    #     pass
-
     def pace_atrium(self,lower_rate_limit):
         interval = self.calc_interval(lower_rate_limit)
         return interval
 
-    def pace_ventricle(self,lower_rate_limit):
+    def pace_ventricular(self,lower_rate_limit):
         interval = self.calc_interval(lower_rate_limit)
         return interval
 
 
-    @staticmethod
-    def sense_atrium(data,heart_data,interval, p_wave, sense_type):
-        has_pace = False
-        p_wave_limit = 100
 
-        # too slow / not sensed
-        if heart_data < 60 or p_wave < data.atrial_sensitivity * p_wave_limit:
+    def sense_atrium(data:IO,interval, p_wave, sense_type):
+        has_sense = False
+
+        # not sensed
+        if p_wave < data.atrial_sensitivity:
             interval = interval-data.atrial_pulse_width
             if sense_type == "trigger":
-                sleep(max(interval,data.ARP)/1000)
-                print("trigger",(max(interval,data.ARP)))
-                has_pace = True
+                # sleep(max(interval,data.ARP)/1000)
+                sleep(data.ARP/1000)
+                print("trigger",data.ARP)
             else:
-                sleep(max(interval,data.ARP,data.PVARP)/1000)
+                sleep(max(data.ARP,data.PVARP)/1000)
+                print("inhibit",data.ARP)
 
-        # too fast
-        # inhibit
-        elif heart_data > 100:
-            sleep(data.ARP/1000)
-            print("too fast")
-            has_pace = True
         # normal
         # continue sense
         else:
-            sleep(data.ARP/1000)
+            # sleep(data.ARP/1000)
+            sleep(interval/1000)
             print("normal")
-        return has_pace
+            has_sense = True
+        return has_sense
 
-    @staticmethod
-    def sense_ventricle(data,heart_data, interval, r_wave, sense_type):
-        r_wave_limit = 100
+
+    def sense_ventricular(data, interval, r_wave, sense_type):
+        has_sense = False
         # too slow / not sensed
-        if heart_data < 60 or r_wave < data.ventricular_sensitivity * r_wave_limit:
+        if r_wave < data.ventricular_sensitivity:
             interval = interval-data.ventricular_pulse_width
             if sense_type == "trigger":
-                sleep(max(interval,data.ARP)/1000)
-                print("trigger",(max(interval,data.VRP,data.PVARP)))
+                sleep(max(data.VRP,data.PVARP)/1000)
+                print("trigger",(max(data.VRP,data.PVARP)))
+            # inhibit
             else:
-                sleep(max(interval,data.VRP)/1000)
+                sleep(data.VRP/1000)
                 print("inhibit")
-        # too fast
         # inhibit
-        elif heart_data > 100:
-            sleep(data.VRP/1000)
-            print("too fast")
         # normal
         # continue sense
         else:
-            sleep(data.VRP/1000)
             print("normal")
+            has_sense = True
+        return has_sense
 
 
-    @staticmethod
+
     def trigger(amplitude,pulse_width):
         print(amplitude,pulse_width)
+        sleep(pulse_width/1000)
+        # TODO: send to lead
         return amplitude,pulse_width
 
-    @staticmethod
-    def inhibit(interval):
-        sleep(interval)
-    #
+    # hystersis is OFF
+    def inhibit(pulse_width):
+
+        print(pulse_width)
+        sleep(pulse_width/1000)
+        # TODO: send NONE to lead
+
+    # hysteresis is not OFF
+    def inhibit(pulse_width,hysteresis,hate_rate):
+        # lower than LRL, larger than hysteresis
+        # 抑制
+
+
+        return None
+
+        # higher than both
+        # 抑制
+
+        # lower than both
+        # 不抑制
+
+
+        print(pulse_width)
+
+
+
     # def set_mode():
     #     pass
     #
--- pacemaker/mode.py
+++ pacemaker/mode.py
@@ -1,188 +1,219 @@
-# from parameters import Parameters
-class Mode:
+import _thread
+from time import sleep
+import IO
+from heart_controller import HeartController
 
-    mode = ["AOO", "VOO", ""]
-    def AAT(pacemaker):
-     return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.atrial_amplitude,
-    pacemaker.atrial_pulse_width,
-    pacemaker.atrial_sensitivity,
-    pacemaker.ARP,
-    pacemaker.PVARP
-    ]
-
-def VVT(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.ventricular_amplitude,
-    pacemaker.ventricular_pulse_width,
-    pacemaker.ventricular_sensitivity,
-    pacemaker.VRP
-    ]
-
-def AOO(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.atrial_amplitude,
-    pacemaker.atrial_pulse_width,
-    ]
+def AAT(self, data: IO):
+    # data.lower_rate_limit
+    # data.upper_rate_limit
+    # data.atrial_amplitude
+    # data.atrial_pulse_width
+    # data.atrial_sensitivity,
+    # data.ARP,
+    # data.PVARP
+    interval = HeartController.pace_atrium(data.lower_rate_limit)
+    # todo get P_wave
+    p_wave = 110
+    has_sense = HeartController.sense_atrium(data, interval, p_wave, "trigger")
+    if not has_sense:
+        HeartController.trigger(data.atrial_amplitude, data.atrial_pulse_width)
+        sleep((interval-data.atrial_pulse_width-data.ARP)/1000)
+    # has_sense
+    else:
+        sleep(interval/1000)
+    return data.atrial_amplitude, data.atrial_pulse_width
 
 
-    # lower rate limit, upper rate limit, atrial amplitude, atrial pulse width
-    # li = []
-    # li.append(Parameters.change_lrl(lower_rate_limit))
-    # set_max_pacing_interval(?)
-    # # 应该定义起搏间隔（没hysteresis）
-    # li.append(Parameters.change_url(upper_rate_limit))
-    # set_min_pacing_interval(?)
-    # li.append(Parameters.change_aa(atrial_amplitude))
-    # # todo 间隔时间
-    # li.append(Parameters.change_apw(atrial_pulse_width))
-    return li
+# while True:
+#     # sensed ventricular
+#     if sensed_ventricular:
+#         continue
+#     interval = 60000
+#
+#     pacemaker.ARP
+#
+# interval = 60000 / pacemaker.lower_rate_limit
+#
+# return pacemaker.atrial_amplitude, pacemaker.atrial_pulse_width,pacemaker.atrial_sensitivity
+
+def VVT(self, data: IO):
+    # pacemaker.lower_rate_limit,
+    # pacemaker.upper_rate_limit,
+    # pacemaker.ventricular_amplitude,
+    # pacemaker.ventricular_pulse_width,
+    # pacemaker.ventricular_sensitivity,
+    # pacemaker.VRP
+    interval = HeartController.pace_ventricular(data.lower_rate_limit)
+    # todo get r_wave
+    r_wave = 110
+    has_sense = HeartController.sense_ventricular(data, interval, r_wave, "trigger")
+    if not has_sense:
+        self.trigger(data.ventricular_amplitude, data.ventricular_pulse_width)
+        sleep((interval-data.ventricular_pulse_width-data.ARP)/1000)
+    # has_sense
+    else:
+        sleep(interval / 1000)
+    return data.ventricular_amplitude, data.ventricular_pulse_width
+
+def AOO(self, data: IO):
+    # pacemaker.lower_rate_limit,
+    # pacemaker.upper_rate_limit,
+    # pacemaker.atrial_amplitude,
+    # pacemaker.atrial_pulse_width,
+
+    interval = self.pace_atrium(data.lower_rate_limit) - data.atrial_pulse_width
+    sleep(interval / 1000)
+    print(data.atrial_amplitude, data.atrial_pulse_width)
+    return data.atrial_amplitude, data.atrial_pulse_width
+
+    # return li
     # todo 两个参数（a_amplitude,a_pulse_width)
 
 
-def AAI(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.atrial_amplitude,
-    pacemaker.atrial_pulse_width,
-    pacemaker.atrial_sensitivity,
-    pacemaker.ARP,
-    pacemaker.PVARP,
-    pacemaker.hysteresis,
-    pacemaker.rate_smoothing
-    ]
-
-def VOO(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.ventricular_amplitude,
-    pacemaker.ventricular_pulse_width
-    ]
-
-def VVI(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.ventricular_amplitude,
-    pacemaker.ventricular_pulse_width,
-    pacemaker.ventricular_sensitivity,
-    pacemaker.VRP,
-    pacemaker.hysteresis,
-    pacemaker.rate_smoothing,
-    ]
-
-def VDD(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.fixed_AV_delay,
-    pacemaker.dynamic_AV_delay,
-    pacemaker.ventricular_amplitude,
-    pacemaker.ventricular_pulse_width,
-    pacemaker.VRP,
-    pacemaker.PVARP_extension,
-    pacemaker.rate_smoothing,
-    pacemaker.atr_duration,
-    pacemaker.atr_fallback_mode,
-    pacemaker.atr_fallback_time
-    ]
-    pass
+def AAI(self, data):
+    #     pacemaker.lower_rate_limit,
+    #     pacemaker.upper_rate_limit,
+    #     pacemaker.atrial_amplitude,
+    #     pacemaker.atrial_pulse_width,
+    #     pacemaker.atrial_sensitivity,
+    #     pacemaker.ARP,
+    #     pacemaker.PVARP,
+    #     pacemaker.hysteresis,
+    #     pacemaker.rate_smoothing
+    interval = HeartController.pace_atrium(data.lower_rate_limit)
+    # todo get p_wave
+    p_wave = 1000
+    has_sense = HeartController.sense_atrium(data, interval, p_wave, "inhibit")
+    if has_sense:
+        # no hysteresis
+        if not data.hysteresis:
+        #     inhibit(pulse_width):
+            HeartController.inhibit(data.atrial_pulse_width)
+        # hysteresis
+        else:
+#            inhibit(pulse_width,hysteresis,hate_rate):
+            HeartController.inhibit(data.atrial_pulse_width,data.hysteresis,data.hate_rate)
+    else:
+        return data.pulse_width,data.hysteresis
 
 
-def DOO(pacemaker):
-    return [
-    pacemaker.lower_rate_limit,
-    pacemaker.upper_rate_limit,
-    pacemaker.fixed_av_delay,
-    pacemaker.atrial_amplitude,
-    pacemaker.ventricular_amplitude,
-    pacemaker.atrial_pulse_width,
-    pacemaker.ventricular_pulse_width
-
-    ]
-
-def DDI(pacemaker):
-    return [
-        pacemaker.lower_rate_limit,
-        pacemaker.upper_rate_limit,
-        pacemaker.fixed_AV_delay,
-        pacemaker.atrial_amplitude,
-        pacemaker.ventricular_amplitude,
-        pacemaker.atrial_pulse_width,
-        pacemaker.ventricular_pulse_width,
-        pacemaker.atrial_sensitivity,
-        pacemaker.ventricular_sensitivity,
-        pacemaker.VRP,
-        pacemaker.ARP,
-        pacemaker.PVARP,
-    ]
+def VOO(self, data: IO):
+    # pacemaker.lower_rate_limit,
+    # pacemaker.upper_rate_limit,
+    # pacemaker.ventricular_amplitude,
+    # pacemaker.ventricular_pulse_width
+    interval = self.pace_ventricular(data.lower_rate_limit) - data.ventricular_pulse_width
+    sleep(interval / 1000)
+    print(data.ventricular_amplitude, data.ventricular_pulse_width)
+    return data.ventricular_amplitude, data.ventricular_pulse_width
 
 
-def DDD(pacemaker):
-    return [
-        pacemaker.lower_rate_limit,
-        pacemaker.upper_rate_limit,
-        pacemaker.fixed_AV_delay,
-        pacemaker.dynamic_AV_delay,
-        pacemaker.sensed_av_delay_offset,
-        pacemaker.atrial_amplitude,
-        pacemaker.ventricular_amplitude,
-        pacemaker.atrial_pulse_width,
-        pacemaker.ventricular_pulse_width,
-        pacemaker.atrial_sensitivity,
-        pacemaker.ventricular_sensitivity,
-        pacemaker.VRP,
-        pacemaker.ARP,
-        pacemaker.PVARP,
-        pacemaker.PVARP_extension,
-        pacemaker.hysteresis,
-        pacemaker.rate_smoothing,
-        pacemaker.atr_duration,
-        pacemaker.atr_fallback_mode,
-        pacemaker.atr_fallback_time
-    ]
-    pass
+def VVI(self, pacemaker):
+    self.pace_ventricular()
+    self.sense_ventricular()
+    self.response_inhibit()
+    # return [
+    #     pacemaker.lower_rate_limit,
+    #     pacemaker.upper_rate_limit,
+    #     pacemaker.ventricular_amplitude,
+    #     pacemaker.ventricular_pulse_width,
+    #     pacemaker.ventricular_sensitivity,
+    #     pacemaker.VRP,
+    #     pacemaker.hysteresis,
+    #     pacemaker.rate_smoothing,
+    # ]
 
 
-def AOOR():
-    pass
+def VDD(self, pacemaker):
+    self.pace_ventricular()
+    self.sense_ventricular()
+    self.sense_atrium()
+    self.response_trigger()
+    self.response_inhibit()
+    # return [
+    #     pacemaker.lower_rate_limit,
+    #     pacemaker.upper_rate_limit,
+    #     pacemaker.fixed_AV_delay,
+    #     pacemaker.dynamic_AV_delay,
+    #     pacemaker.ventricular_amplitude,
+    #     pacemaker.ventricular_pulse_width,
+    #     pacemaker.VRP,
+    #     pacemaker.PVARP_extension,
+    #     pacemaker.rate_smoothing,
+    #     pacemaker.atr_duration,
+    #     pacemaker.atr_fallback_mode,
+    #     pacemaker.atr_fallback_time
+    # ]
+    # pass
 
 
-def AAIR():
-    pass
+def DOO(self, data):
+    try:
+        _thread.start_new_thread(self.AOO, (data,))
+        _thread.start_new_thread(self.VOO, (data,))
+    except Exception as ex:
+        print(ex)
+    # return [
+    #     pacemaker.lower_rate_limit,
+    #     pacemaker.upper_rate_limit,
+    #     pacemaker.fixed_av_delay,
+    #     pacemaker.atrial_amplitude,
+    #     pacemaker.ventricular_amplitude,
+    #     pacemaker.atrial_pulse_width,
+    #     pacemaker.ventricular_pulse_width
+    #
+    # ]
 
 
-def VOOR():
-    pass
+def DDI(self, pacemaker):
+    self.pace_atrium()
+    self.pace_ventricular()
+    self.sense_atrium()
+    self.sense_ventricular()
+    self.response_inhibit()
+    # return [
+    #     pacemaker.lower_rate_limit,
+    #     pacemaker.upper_rate_limit,
+    #     pacemaker.fixed_AV_delay,
+    #     pacemaker.atrial_amplitude,
+    #     pacemaker.ventricular_amplitude,
+    #     pacemaker.atrial_pulse_width,
+    #     pacemaker.ventricular_pulse_width,
+    #     pacemaker.atrial_sensitivity,
+    #     pacemaker.ventricular_sensitivity,
+    #     pacemaker.VRP,
+    #     pacemaker.ARP,
+    #     pacemaker.PVARP,
+    # ]
 
 
-def VVIR():
-    pass
-
-
-
-def VDDR():
-    pass
-
-
-
-
-def DOOR():
-    pass
-
-
-def DDIR():
-    pass
-
-def DDDR():
-    pass
-
-
+def DDD(self, pacemaker):
+    self.pace_ventricular()
+    self.pace_ventricular()
+    self.sense_atrium()
+    self.sense_ventricular()
+    self.response_trigger()
+    self.response_inhibit()
+    # return [
+    #     pacemaker.lower_rate_limit,
+    #     pacemaker.upper_rate_limit,
+    #     pacemaker.fixed_AV_delay,
+    #     pacemaker.dynamic_AV_delay,
+    #     pacemaker.sensed_av_delay_offset,
+    #     pacemaker.atrial_amplitude,
+    #     pacemaker.ventricular_amplitude,
+    #     pacemaker.atrial_pulse_width,
+    #     pacemaker.ventricular_pulse_width,
+    #     pacemaker.atrial_sensitivity,
+    #     pacemaker.ventricular_sensitivity,
+    #     pacemaker.VRP,
+    #     pacemaker.ARP,
+    #     pacemaker.PVARP,
+    #     pacemaker.PVARP_extension,
+    #     pacemaker.hysteresis,
+    #     pacemaker.rate_smoothing,
+    #     pacemaker.atr_duration,
+    #     pacemaker.atr_fallback_mode,
+    #     pacemaker.atr_fallback_time
+    # ]
+    # pass
