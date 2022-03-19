8904cf8ce8bfbf8fd89e844a96299efe03297ee8
Chenyu <machenyu96@gmail.com>
2022-03-19 21:33:56 +0000

    feat:add 2 test class

--- pacemaker/io.py
+++ pacemaker/io.py
@@ -4,86 +4,44 @@
 # In[5]:
 
 
-class InputData:
+class IO:
     
     
-    def __init__(self, lower_rate_limit , upper_rate_limit, fixed_AV_delay , dynamic_AV_delay , sensed_AV_delay_offset,atrial_amplitude
-                ,ventricular_amplitude, atrial_pulse_width, ventricular_pulse_width, atrial_sensitivity, ventricular_sensitivity
-                ,VRP, ARP, hysteresis, PVARP, PVARP_extension,
-                 rate_smoothing, ATR_duration, ATR_fallback_mode,ATR_fallback_time):
+    def __init__(self, LowerRateLimit , UpperRateLimit, FixedAVDelay , DynamicAVDelay , SensedAVDelayOffset,AtrialAmplitude
+                ,VentricualrAmplitude, AtrialPulseWidth, VentricularPulseWidth, AtrialSensitivity, VentricularSensitivity
+                ,VRP, ARP, Hysteresis, PVARP, PVARPExtension, RateSmoothing, ATRDuration, ATRFallbackMode,ATRFallbackTime):
         
-        self.lower_rate_limit = lower_rate_limit
-        self.upper_rate_limit = upper_rate_limit
-        self.fixed_AV_delay = fixed_AV_delay
-        self.dynamic_AV_delay = dynamic_AV_delay
-        self.sensed_AV_delay_offset = sensed_AV_delay_offset
-        self.atrial_amplitude = atrial_amplitude
-        self.ventricular_amplitude = ventricular_amplitude
-        self.atrial_pulse_width = atrial_pulse_width
-        self.ventricular_pulse_width = ventricular_pulse_width
-        self.atrial_sensitivity = atrial_sensitivity
-        self.ventricular_sensitivity = ventricular_sensitivity
+        self.LowerRateLimit = LowerRateLimit
+        self.UpperRateLimit = UpperRateLimit
+        self.FixedAVDelay = FixedAVDelay
+        self.DynamicAVDelay = DynamicAVDelay
+        self.SensedAVDelayOffset = SensedAVDelayOffset
+        self.AtrialAmplitude = AtrialAmplitude
+        self.VentricualrAmplitude = VentricualrAmplitude
+        self.AtrialPulseWidth = AtrialPulseWidth
+        self.VentricularPulseWidth = VentricularPulseWidth
+        self.AtrialSensitivity = AtrialSensitivity
+        self.VentricularSensitivity = VentricularSensitivity
         self.VRP = VRP
         self.ARP = ARP
-        self.hysteresis = hysteresis
+        self.Hysteresis = Hysteresis
         self.PVARP = PVARP
-        self.PVARP_extension = PVARP_extension
-        self.rate_smoothing = rate_smoothing
-        self.ATR_duration = ATR_duration
-        self.ATR_fallback_mode = ATR_fallback_mode
-        self.ATR_fallback_time = ATR_fallback_time
+        self.PVARPExtension = PVARPExtension
+        self.RateSmoothing = RateSmoothing
+        self.ATRDuration = ATRDuration
+        self.ATRFallbackMode = ATRFallbackMode
+        self.ATRFallbackTime = ATRFallbackTime
         
     def check_values(self):
     
         
-        param = [self.lower_rate_limit , self.upper_rate_limit, self.fixed_AV_delay , self.dynamic_AV_delay , self.sensed_AV_delay_offset,self.atrial_amplitude
-                ,self.ventricular_amplitude, self.atrial_pulse_width, self.ventricular_pulse_width, self.atrial_sensitivity, self.ventricular_sensitivity
-                ,self.VRP, self.ARP, self.hysteresis, self.PVARP, self.PVARP_extension, self.rate_smoothing, self.ATR_duration, self.ATR_fallback_mode,self.ATR_fallback_time]
-
-
-        # minimum_dynamic_AV_delay? never use? maximum_sensor
-        # int
-        # lower_rate_limit,upper_rate_limit,,fixed_AV_delay,
-        # VRP, ARP, PVARP, ATR_ duration,  ATR fallback time
-        for param in [self.lower_rate_limit,self.upper_rate_limit,self.fixed_AV_delay,self.VRP,self.ARP,self.PVARP,self.ATR_duration,self.ATR_duration,self.ATR_fallback_time]:
-            self.lower_rate_limit = int(self.lower_rate_limit)
-
-
-        # float
-        # atrial_pulse_width, ventricular_pulse_width
-        # atrial_sensitivity, ventricular_sensitivity
-        for param in [self.atrial_pulse_width,self.ventricular_pulse_width,self.atrial_sensitivity,self.ventricular_sensitivity]:
-
-
-
-        # bool
-        # dynamic_AV_delay, atr_fallback_mode
-
-        # "OFF" or int
-        # sensed_AV_delay_offset, PVARP_extension, rate_smoothing(?有无%)
-
-        # "OFF" or float
-        # atrial_amplitude, ventricular_amplitude,
-
-
-
+        param = [self.LowerRateLimit , self.UpperRateLimit, self.FixedAVDelay , self.DynamicAVDelay , self.SensedAVDelayOffset,self.AtrialAmplitude
+                ,self.VentricualrAmplitude, self.AtrialPulseWidth, self.VentricularPulseWidth, self.AtrialSensitivity, self.VentricularSensitivity
+                ,self.VRP, self.ARP, self.Hysteresis, self.PVARP, self.PVARPExtension, self.RateSmoothing, self.ATRDuration, self.ATRFallbackMode,self.ATRFallbackTime]
+        
         for par in param:
-            if par == "OFF":
-                print('OFF')
+            if par == None:
+                print('None')
             else:
-                # par = int()
-                print(par)
-
-if __name__ == '__main__':
-    # "60","120","150","OFF","OFF","3.5",
-    # "3.5","0.4","0.4","0.75","2.5",
-    # "320","250","OFF","250","OFF",
-    # "OFF","OFF","20","1"
-
-    # io = IO("60","120","150","OFF","OFF","3.5",
-    #         "3.5","0.4","0.4","0.75","2.5",
-    #         "320","250","OFF","250","OFF",
-    #         "OFF","OFF","20","1")
-    io2 = InputData(60,120,150,"OFF","OFF",3.5,3.5,0.4,0.4,0.75,2.5,320,250,"OFF",250,"OFF","OFF","OFF",20,1)
-
-    io2.check_values()
+                print(int(par))
+  
